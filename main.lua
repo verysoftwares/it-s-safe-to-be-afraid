0b1048d079bb02058bb7e3381a3cc49b7330807c
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2025-12-14 14:44:48 +0200

    v2.4

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+-- alias
+    lg = love.graphics
+    sin = math.sin; cos = math.cos
+
 require 'load'
 
 t = 0
@@ -11,6 +15,23 @@ function love.update()
     t = t+1
 end
 
+asset.draw = {}
+asset.draw['maincanvas'] = lg.newCanvas(SW,SH)
+asset.draw.color = {}
+asset.draw.color['BG'] = {r=0.33,g=0.25,b=0.4}
+asset.draw.color['FG'] = {r=0.25*3,g=0.4,b=0.33*2}
+
 function love.draw()
-    
+    lg.setCanvas(asset.draw['maincanvas'])
+        local bgc = asset.draw.color['BG']
+            lg.setBackgroundColor(bgc.r,bgc.g,bgc.b)
+            lg.clear()
+        local fgc = asset.draw.color['FG']
+            lg.setColor(fgc.r,fgc.g,fgc.b)
+            lg.rectangle('fill',0+120,0+120,SW-120*2,SH-120*2)
+            -- why does this give me black??
+                --lg.setColor(bgc.r,bgc.g,bgc.b)
+                --lg.rectangle('fill',0+120*1.5,0+120*1.5,SW-120*1.5*2,SH-120*1.5*2)
+    lg.setCanvas()
+        lg.draw(asset.draw['maincanvas'])
 end
